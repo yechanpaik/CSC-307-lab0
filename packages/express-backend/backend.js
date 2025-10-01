e54210514a101e3a9291dc224228d06bad7c4c9b
sean <yepaik@calpoly.edu>
2025-10-01 11:10:26 -0700

    id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,6 +40,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -52,6 +55,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
