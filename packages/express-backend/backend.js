35a31aa80f0fed2f8a9748883960b61ab5d1fea2
sean <yepaik@calpoly.edu>
2025-10-01 12:27:48 -0700

    delete not working

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -56,6 +61,12 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.get("/users/:id", (req, res) => {
